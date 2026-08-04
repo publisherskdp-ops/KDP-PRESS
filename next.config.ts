913f8d8dd8e4462c14b131d8c038a2f4607b5db4
TheSamsDev <razasamee35@gmail.com>
2026-08-04 18:44:19 +0500

    cahges

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
-/**
- * Next.js configuration with development enhancements.
- * - allowedDevOrigins: permits local network access during dev.
- */
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.113"],
+
   experimental: {
     serverActions: {
-      bodySizeLimit: '100mb',
+      bodySizeLimit: "300mb",
     },
+
+    middlewareClientMaxBodySize: "300mb",
   },
 };
 
